8959bbd47d8d803f29df8347f37e48e18f6c58c9
jjmin9797 <jjmin9797@naver.com>
2023-03-21 15:58:29 +0900

    today

--- 20230320/1715.py
+++ 20230320/1715.py
@@ -7,13 +7,15 @@ cards = [int(input()) for _ in range(n)]
 heap = []
 for card in cards:
     heappush(heap, card)
-
-answer = 0
-while len(heap) > 2:
-    firstCard = heappop(heap)
-    secondCard = heappop(heap)
-    nextCard = firstCard + secondCard
-    answer += nextCard
-    heappush(heap, nextCard)
-print(answer)
-print(heap)
+if len(cards) == 1 :
+    print(0)
+else :
+    answer = 0
+    while len(heap) > 2:
+        firstCard = heappop(heap)
+        secondCard = heappop(heap)
+        nextCard = firstCard + secondCard
+        answer += nextCard
+        heappush(heap, nextCard)
+    answer += sum(heap)
+    print(answer)
